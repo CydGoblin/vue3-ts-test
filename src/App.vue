440e09d302a897dc35a7059554bc08a50b6faa3a
Luz Virginia Aramburo Alvarez <265+l-aramburo@users.noreply.github.palaceresorts.com>
2022-11-05 09:26:33 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AddVideoInput from "@/components/AddVideoInput.vue";
+</script>
+
+<template>
+  <header></header>
+
+  <main class="main">
+    <h1>Añadir nuevo video</h1>
+    <AddVideoInput />
+  </main>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/AddVideoInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <input type="text" />
+    <button>Añadir</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "AddVideoInput",
+};
+</script>
